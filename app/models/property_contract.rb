5852e35f6539f8758908f3f7c2d9410f0c7c0c60
plmaurel69 <88190008+plmaurel69@users.noreply.github.com>
2022-01-29 16:01:41 +0100

    setup relationship

--- app/models/property_contract.rb
+++ app/models/property_contract.rb
@@ -1,4 +1,7 @@
 class PropertyContract < ApplicationRecord
   belongs_to :property
   belongs_to :user
+
+  has_many :balance_sheets
+  has_many :messages
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :messages
+  has_many :properties #as an owner
+  has_many :property_contracts
+  has_many :properties_as_a_renter, through: :property_contracts, source: :property #as a renter
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
