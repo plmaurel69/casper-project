2ebfd9963e0466375eb4ad46769cb13809c5a716
nicolas jambon <nicolas_jambon@hotmail.fr>
2022-02-01 21:18:50 +0100

    Changement database BalanceSheet

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :properties, only: [:show, :index, :new, :create, :edit] do
     resources :messages, only: [:index, :create]
-    resources :balance_sheets, only: [:index, :show, :update]
+    resources :balance_sheets, only: [:index, :show, :update, :create]
   end
 end
