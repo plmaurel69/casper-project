204d7a406754d7e8c12c81fa4976d1888c761e96
plmaurel69 <88190008+plmaurel69@users.noreply.github.com>
2022-02-05 11:54:40 +0100

    setup finish

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,8 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "fullcalendar"
+import "chartkick/chart.js"
+
 
 Rails.start()
 Turbolinks.start()
