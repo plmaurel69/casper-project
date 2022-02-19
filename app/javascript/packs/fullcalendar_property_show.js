20f56c02a6bc6cc48b0cc4eab25c2cd39eec2ceb
Sanae Benali <sanae.benali@gmail.com>
2022-02-19 15:20:10 +0100

    change in home and index

--- app/javascript/packs/fullcalendar_property_show.js
+++ app/javascript/packs/fullcalendar_property_show.js
@@ -24,36 +24,51 @@ const setCalendarPropertyShow = () => {
       {
         title: 'Plumber Flat #1',
         start: '2022-02-02',
+        backgroundColor: '#081A51',
+        borderColor: '#081A51',
       },
       {
         title: 'Construction Flat #2',
         start: '2022-02-07',
         end: '2022-02-10',
+        backgroundColor: '#081A51',
+        borderColor: '#081A51',
       },
       {
         title: 'Pay Rent Day',
-        start: '2022-02-09'
+        start: '2022-02-09',
+        backgroundColor: '#081A51',
+        borderColor: '#081A51',
       },
       {
         title: 'Conference Landlords',
         start: '2022-02-14',
+        backgroundColor: '#081A51',
       },
       {
         title: 'Buy new flat',
-        start: '2022-02-14'
+        start: '2022-02-14',
+        backgroundColor: '#081A51',
+        borderColor: '#081A51',
       },
       {
         title: 'OFF Vacation',
         start: '2022-02-15',
-        end: '2022-02-25'
+        end: '2022-02-25',
+        backgroundColor: '#081A51',
+        borderColor: '#081A51',
       },
       {
         title: 'Pay Rent Day',
-        start: '2022-03-09'
+        start: '2022-03-09',
+        backgroundColor: '#081A51',
+        borderColor: '#081A51',
       },
       {
         title: 'End of Contract Flat #3',
-        start: '2022-03-20'
+        start: '2022-03-20',
+        backgroundColor: '#081A51',
+        borderColor: '#081A51',
       },
     ],
   });
